62d250ccb11fd1155b8bbdca50a492e04ed37866
anniedelgee <117107188+anniedelgee@users.noreply.github.com>
2022-11-13 18:43:06 +0000

    JScript functions added to console

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,27 @@
 var topics= ['HTML', 'CSS', 'Git', 'Javascript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (var x=0; x<topics.length; x++) {
-    console.log(topics[x]);
+function listTopic() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
-/*
-if (topics==='HTML') {
+listTopic();
+function selectTopic(){
+    if (randomTopic==='HTML') {
     console.log("Let's study HTML!");
-} else if (topics ==='CSS') {
+} else if (randomTopic ==='CSS') {
     console.log ("Let's study CSS!");
-} else if (topics==='Git') {
-    console.log("Let's study Git!")
-} else if (topics==='Javascript') {
+} else if (randomTopic==='Git') {
+    console.log("Let's study Git!");
+} else if (randomTopic==='Javascript') {
     console.log("Let's study Javascript!");
 } else {
     console.log('Please try again!');
 }
-*/
+}
+console.log('Here are the topics we learned through Prework');
+listTopic();
+console.log('Which topic should we study first?');
+selectTopic();
+
